8fca19499ae77bac2c64a22098e6fb8ea24709ae
JoeyJaySWE <joeyjay@joeyjaydigital.com>
2021-05-26 19:46:56 +0200

    Update server.js
    tested neew code

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const server = require('express')();
-const http = require('http').createServer(server);
-
-// io.on('connection', (socket) => {
-//     console.log('A user connectd: ' + socket.id);
-
-//     io.on('disconnect', (socket) => {
-//         console.log('A user diconnected: ' + socket.id);
-//     });
-// });
+const http = require('http');
+const httpServer = http.createServer();
+const { Server } = require('socket.io');
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  },
+});
 
 const port = process.env.PORT || 8000;
-const io = require('./socket').init(http);
 
-const serverListner = http.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('Connected to the socket server');
+});
+
+httpServer.listen(port, () => {
   console.log('Server started up');
 });
